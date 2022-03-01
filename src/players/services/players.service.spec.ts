eae8c9df7fe90bd428d3eb0b240d25be776814bf
Bruna Lopes <bruna-chan@live.com>
2022-03-01 13:33:49 -0300

    fix: player service unit test

--- src/players/services/players.service.spec.ts
+++ src/players/services/players.service.spec.ts
@@ -1,12 +1,28 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlayersService } from './players.service';
 
 describe('PlayersService', () => {
   let service: PlayersService;
 
+  const mockModel = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    findOneById: jest.fn(),
+    save: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+    deleteOne: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PlayersService],
+      providers: [
+        PlayersService,
+        {
+          provide: getModelToken('Player'),
+          useValue: mockModel,
+        },
+      ],
     }).compile();
 
     service = module.get<PlayersService>(PlayersService);
